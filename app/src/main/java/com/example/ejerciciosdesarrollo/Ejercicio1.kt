4df766b8d901249565e7ba121494ecb52a24db3c
EAG <Mataramosmiguel@gmail.com>
2023-11-02 19:00:30 +0100

    vamo avanzando y ordenao

--- app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio1.kt
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio1.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
 import kotlin.random.Random
 
 class Ejercicio1 : AppCompatActivity() {
@@ -37,7 +39,8 @@ class Ejercicio1 : AppCompatActivity() {
 
             while (jugador){
                 when (view.id){
-                    R.id.b1 -> {var b1 = findViewById<ImageView>(R.id.b1)
+                    R.id.b1 -> {
+                        var b1 = findViewById<ImageView>(R.id.b1)
                         if (tablero2[0]==0){
                             b1.setImageResource(R.drawable.x)
                             tablero2[0] = 1
@@ -47,7 +50,8 @@ class Ejercicio1 : AppCompatActivity() {
 
                     }
 
-                    R.id.b2 -> {var b2 = findViewById<ImageView>(R.id.b2)
+                    R.id.b2 -> {
+                        var b2 = findViewById<ImageView>(R.id.b2)
                         if (tablero2[1]==0){
                             b2.setImageResource(R.drawable.x)
                             tablero2[1] = 1
@@ -57,7 +61,8 @@ class Ejercicio1 : AppCompatActivity() {
 
                     }
 
-                    R.id.b3 -> {var b3 = findViewById<ImageView>(R.id.b3)
+                    R.id.b3 -> {
+                        var b3 = findViewById<ImageView>(R.id.b3)
                         if (tablero2[2]==0){
                             b3.setImageResource(R.drawable.x)
                             tablero2[2] = 1
@@ -67,7 +72,8 @@ class Ejercicio1 : AppCompatActivity() {
 
                     }
 
-                    R.id.b4 -> {var b4 = findViewById<ImageView>(R.id.b4)
+                    R.id.b4 -> {
+                        var b4 = findViewById<ImageView>(R.id.b4)
                         if (tablero2[3]==0){
                             b4.setImageResource(R.drawable.x)
                             tablero2[3] = 1
@@ -77,7 +83,8 @@ class Ejercicio1 : AppCompatActivity() {
 
                     }
 
-                    R.id.b5 -> { var b5 = findViewById<ImageView>(R.id.b5)
+                    R.id.b5 -> {
+                        var b5 = findViewById<ImageView>(R.id.b5)
                         if (tablero2[4] == 0) {
                             b5.setImageResource(R.drawable.x)
                             tablero2[4] = 1
@@ -85,7 +92,8 @@ class Ejercicio1 : AppCompatActivity() {
                         }
                     }
 
-                    R.id.b6 -> { var b6 = findViewById<ImageView>(R.id.b6)
+                    R.id.b6 -> {
+                        var b6 = findViewById<ImageView>(R.id.b6)
                         if (tablero2[5] == 0) {
                             b6.setImageResource(R.drawable.x)
                             tablero2[5] = 1
@@ -93,7 +101,8 @@ class Ejercicio1 : AppCompatActivity() {
                         }
                     }
 
-                    R.id.b7 -> { var b7 = findViewById<ImageView>(R.id.b7)
+                    R.id.b7 -> {
+                        var b7 = findViewById<ImageView>(R.id.b7)
                         if (tablero2[6] == 0) {
                             b7.setImageResource(R.drawable.x)
                             tablero2[6] = 1
@@ -101,7 +110,8 @@ class Ejercicio1 : AppCompatActivity() {
                         }
                     }
 
-                    R.id.b8 -> { var b8 = findViewById<ImageView>(R.id.b8)
+                    R.id.b8 -> {
+                        var b8 = findViewById<ImageView>(R.id.b8)
                         if (tablero2[7] == 0) {
                             b8.setImageResource(R.drawable.x)
                             tablero2[7] = 1
@@ -109,7 +119,8 @@ class Ejercicio1 : AppCompatActivity() {
                         }
                     }
 
-                    R.id.b9 -> { var b9 = findViewById<ImageView>(R.id.b9)
+                    R.id.b9 -> {
+                        var b9 = findViewById<ImageView>(R.id.b9)
                         if (tablero2[8] == 0) {
                             b9.setImageResource(R.drawable.x)
                             tablero2[8] = 1
@@ -122,88 +133,101 @@ class Ejercicio1 : AppCompatActivity() {
             }
 
 
-            while (maquina){
+            while (maquina && !jugador){
                 var aleatorio = Random.nextInt(9)
                 when (aleatorio){
-                    0 -> { if (tablero2[0]==0){
+                    0 -> {
+                        if (tablero2[0]==0){
                             var b1 = findViewById<ImageView>(R.id.b1)
                             b1.setImageResource(R.drawable.o)
+                            tablero2[0] = -1
+                            maquina = false
+                        }
+                    }
+
+                    1 -> {
+                        if (tablero2[1]==0){
+                            var b2 = findViewById<ImageView>(R.id.b2)
+                            b2.setImageResource(R.drawable.o)
+                            tablero2[1] = -1
                             maquina = false
                         }
 
                     }
-                    1 -> { if (tablero2[1]==0){
-                        var b2 = findViewById<ImageView>(R.id.b2)
-                        b2.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
+
+                    2 -> {
+                        if (tablero2[2]==0){
+                            var b3 = findViewById<ImageView>(R.id.b3)
+                            tablero2[2] = -1
+                            b3.setImageResource(R.drawable.o)
+                            maquina = false
+                        }
 
                     }
 
-                    2 -> { if (tablero2[2]==0){
-                        var b3 = findViewById<ImageView>(R.id.b3)
-                        b3.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
+                    3 -> {
+                        if (tablero2[3]==0){
+                            var b4 = findViewById<ImageView>(R.id.b4)
+                            b4.setImageResource(R.drawable.o)
+                            tablero2[3] = -1
+                            maquina = false
+                        }
 
                     }
 
-                    3 -> { if (tablero2[3]==0){
-                        var b4 = findViewById<ImageView>(R.id.b4)
-                        b4.setImageResource(R.drawable.o)
-                        maquina = false
+                    4 -> {
+                        if (tablero2[4]==0){
+                            var b5 = findViewById<ImageView>(R.id.b5)
+                            b5.setImageResource(R.drawable.o)
+                            tablero2[4] = -1
+                            maquina = false
+                        }
                     }
+
+                    5 -> {
+                        if (tablero2[5]==0){
+                            var b6 = findViewById<ImageView>(R.id.b6)
+                            b6.setImageResource(R.drawable.o)
+                            tablero2[5] = -1
+                            maquina = false
+                        }
 
                     }
 
-                    4 -> { if (tablero2[4]==0){
-                        var b5 = findViewById<ImageView>(R.id.b5)
-                        b5.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
+                    6 -> {
+                        if (tablero2[6]==0){
+                            var b7 = findViewById<ImageView>(R.id.b7)
+                            b7.setImageResource(R.drawable.o)
+                            tablero2[6] = -1
+                            maquina = false
+                        }
 
                     }
 
-                    5 -> { if (tablero2[5]==0){
-                        var b6 = findViewById<ImageView>(R.id.b6)
-                        b6.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
+                    7 -> {
+                        if (tablero2[7]==0){
+                            var b8 = findViewById<ImageView>(R.id.b8)
+                            b8.setImageResource(R.drawable.o)
+                            tablero2[7] = -1
+                            maquina = false
+                        }
 
                     }
 
-                    6 -> { if (tablero2[6]==0){
-                        var b7 = findViewById<ImageView>(R.id.b7)
-                        b7.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
+                    8 -> {
+                        if (tablero2[8]==0){
+                            var b9 = findViewById<ImageView>(R.id.b9)
+                            b9.setImageResource(R.drawable.o)
+                            tablero2[8] = -1
+                            maquina = false
+                        }
 
                     }
-
-                    7 -> { if (tablero2[7]==0){
-                        var b8 = findViewById<ImageView>(R.id.b8)
-                        b8.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
-
-                    }
-
-                    8 -> { if (tablero2[8]==0){
-                        var b9 = findViewById<ImageView>(R.id.b9)
-                        b9.setImageResource(R.drawable.o)
-                        maquina = false
-                    }
-
-                    }
-
 
                 }
 
+
             }
-
-            terminado = comprobarFin()
-
-
 
         }
 
@@ -212,7 +236,11 @@ class Ejercicio1 : AppCompatActivity() {
 
     }
 
+    fun reset(){
+        recreate()
     }
+
+}
 
 
 
