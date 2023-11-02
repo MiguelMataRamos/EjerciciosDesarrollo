a5737a7c5a3703b475e45ba7d659ce411422c197
EAG <Mataramosmiguel@gmail.com>
2023-11-02 20:24:35 +0100

    pff nose

--- app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio1.kt
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio1.kt
@@ -3,14 +3,12 @@ package com.example.ejerciciosdesarrollo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import kotlin.random.Random
 
 class Ejercicio1 : AppCompatActivity() {
 
-    private var tablero2 = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
+    private var tablero2 = mutableListOf(0,0,0,0,0,0,0,0,0)
     private var terminado = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,223 +17,223 @@ class Ejercicio1 : AppCompatActivity() {
 
     }
 
-    fun comprobarFin(): Boolean {
-        for (i in tablero2) {
-            if (i == 0) {
-                return false
+    fun comprobarFin():Boolean{
+        for (i in tablero2){
+            if (i == 0){
+                return  false
             }
         }
         return true
     }
 
-    fun clik(view: View) {
-        if (!terminado) {
+    fun clik (view:View){
+        if (!terminado){
             var jugador = true
             var maquina = true
 
+            while (jugador){
+                when (view.id){
+                    R.id.b1 -> {
+                        val b1 = findViewById<ImageView>(R.id.b1)
+                        if (tablero2[0]==0){
+                            b1.setImageResource(R.drawable.x)
+                            tablero2[0] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
 
-            when (view.id) {
-                R.id.b1 -> {
-                    val b1 = findViewById<ImageView>(R.id.b1)
-                    if (tablero2[0] == 0) {
-                        b1.setImageResource(R.drawable.x)
-                        tablero2[0] = 1
-                        jugador = false
+                    }
+
+                    R.id.b2 -> {
+                        val b2 = findViewById<ImageView>(R.id.b2)
+                        if (tablero2[1]==0){
+                            b2.setImageResource(R.drawable.x)
+                            tablero2[1] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+
+
+                    }
+
+                    R.id.b3 -> {
+                        val b3 = findViewById<ImageView>(R.id.b3)
+                        if (tablero2[2]==0){
+                            b3.setImageResource(R.drawable.x)
+                            tablero2[2] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+
+
+                    }
+
+                    R.id.b4 -> {
+                        val b4 = findViewById<ImageView>(R.id.b4)
+                        if (tablero2[3]==0){
+                            b4.setImageResource(R.drawable.x)
+                            tablero2[3] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+
+
+                    }
+
+                    R.id.b5 -> { val b5 = findViewById<ImageView>(R.id.b5)
+                        if (tablero2[4] == 0) {
+                            b5.setImageResource(R.drawable.x)
+                            tablero2[4] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+                    }
+
+                    R.id.b6 -> { val b6 = findViewById<ImageView>(R.id.b6)
+                        if (tablero2[5] == 0) {
+                            b6.setImageResource(R.drawable.x)
+                            tablero2[5] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+                    }
+
+                    R.id.b7 -> { val b7 = findViewById<ImageView>(R.id.b7)
+                        if (tablero2[6] == 0) {
+                            b7.setImageResource(R.drawable.x)
+                            tablero2[6] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+                    }
+
+                    R.id.b8 -> { val b8 = findViewById<ImageView>(R.id.b8)
+                        if (tablero2[7] == 0) {
+                            b8.setImageResource(R.drawable.x)
+                            tablero2[7] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
+                    }
+
+                    R.id.b9 -> { val b9 = findViewById<ImageView>(R.id.b9)
+                        if (tablero2[8] == 0) {
+                            b9.setImageResource(R.drawable.x)
+                            tablero2[8] = 1
+                            jugador = false
+                        }else{
+                            break
+                        }
                     }
 
 
-                }
-
-                R.id.b2 -> {
-                    val b2 = findViewById<ImageView>(R.id.b2)
-                    if (tablero2[1] == 0) {
-                        b2.setImageResource(R.drawable.x)
-                        tablero2[1] = 1
-                        jugador = false
-                    }
-
-
-                }
-
-                R.id.b3 -> {
-                    val b3 = findViewById<ImageView>(R.id.b3)
-                    if (tablero2[2] == 0) {
-                        b3.setImageResource(R.drawable.x)
-                        tablero2[2] = 1
-                        jugador = false
-                    }
-
-
-                }
-
-                R.id.b4 -> {
-                    val b4 = findViewById<ImageView>(R.id.b4)
-                    if (tablero2[3] == 0) {
-                        b4.setImageResource(R.drawable.x)
-                        tablero2[3] = 1
-                        jugador = false
-                    }
-
-
-                }
-
-                R.id.b5 -> {
-                    val b5 = findViewById<ImageView>(R.id.b5)
-                    if (tablero2[4] == 0) {
-                        b5.setImageResource(R.drawable.x)
-                        tablero2[4] = 1
-                        jugador = false
-                    }
-                }
-
-                R.id.b6 -> {
-                    val b6 = findViewById<ImageView>(R.id.b6)
-                    if (tablero2[5] == 0) {
-                        b6.setImageResource(R.drawable.x)
-                        tablero2[5] = 1
-                        jugador = false
-                    }
-                }
-
-                R.id.b7 -> {
-                    val b7 = findViewById<ImageView>(R.id.b7)
-                    if (tablero2[6] == 0) {
-                        b7.setImageResource(R.drawable.x)
-                        tablero2[6] = 1
-                        jugador = false
-                    }
-                }
-
-                R.id.b8 -> {
-                    val b8 = findViewById<ImageView>(R.id.b8)
-                    if (tablero2[7] == 0) {
-                        b8.setImageResource(R.drawable.x)
-                        tablero2[7] = 1
-                        jugador = false
-                    }
-                }
-
-                R.id.b9 -> {
-                    val b9 = findViewById<ImageView>(R.id.b9)
-                    if (tablero2[8] == 0) {
-                        b9.setImageResource(R.drawable.x)
-                        tablero2[8] = 1
-                        jugador = false
-                    }
                 }
 
 
             }
 
-
-
-            while (maquina && !jugador) {
+            while (maquina && !jugador){
                 var aleatorio = Random.nextInt(9)
-                when (aleatorio) {
-                    0 -> {
-                        if (tablero2[0] == 0) {
+                when (aleatorio){
+                    0 -> { if (tablero2[0]==0){
                             val b1 = findViewById<ImageView>(R.id.b1)
                             b1.setImageResource(R.drawable.o)
                             tablero2[0] = -1
                             maquina = false
                         }
-                    }
-
-                    1 -> {
-                        if (tablero2[1] == 0) {
-                            val b2 = findViewById<ImageView>(R.id.b2)
-                            b2.setImageResource(R.drawable.o)
-                            tablero2[1] = -1
-                            maquina = false
-                        }
 
                     }
-
-                    2 -> {
-                        if (tablero2[2] == 0) {
-                            val b3 = findViewById<ImageView>(R.id.b3)
-                            tablero2[2] = -1
-                            b3.setImageResource(R.drawable.o)
-                            maquina = false
-                        }
+                    1 -> { if (tablero2[1]==0){
+                        val b2 = findViewById<ImageView>(R.id.b2)
+                        b2.setImageResource(R.drawable.o)
+                        tablero2[1] = -1
+                        maquina = false
+                    }
 
                     }
 
-                    3 -> {
-                        if (tablero2[3] == 0) {
-                            val b4 = findViewById<ImageView>(R.id.b4)
-                            b4.setImageResource(R.drawable.o)
-                            tablero2[3] = -1
-                            maquina = false
-                        }
+                    2 -> { if (tablero2[2]==0){
+                        val b3 = findViewById<ImageView>(R.id.b3)
+                        tablero2[2] = -1
+                        b3.setImageResource(R.drawable.o)
+                        maquina = false
+                    }
 
                     }
 
-                    4 -> {
-                        if (tablero2[4] == 0) {
-                            val b5 = findViewById<ImageView>(R.id.b5)
-                            b5.setImageResource(R.drawable.o)
-                            tablero2[4] = -1
-                            maquina = false
-                        }
+                    3 -> { if (tablero2[3]==0){
+                        val b4 = findViewById<ImageView>(R.id.b4)
+                        b4.setImageResource(R.drawable.o)
+                        tablero2[3] = -1
+                        maquina = false
                     }
-
-                    5 -> {
-                        if (tablero2[5] == 0) {
-                            val b6 = findViewById<ImageView>(R.id.b6)
-                            b6.setImageResource(R.drawable.o)
-                            tablero2[5] = -1
-                            maquina = false
-                        }
 
                     }
 
-                    6 -> {
-                        if (tablero2[6] == 0) {
-                            val b7 = findViewById<ImageView>(R.id.b7)
-                            b7.setImageResource(R.drawable.o)
-                            tablero2[6] = -1
-                            maquina = false
-                        }
+                    4 -> { if (tablero2[4]==0){
+                        val b5 = findViewById<ImageView>(R.id.b5)
+                        b5.setImageResource(R.drawable.o)
+                        tablero2[4] = -1
+                        maquina = false
+                    }
 
                     }
 
-                    7 -> {
-                        if (tablero2[7] == 0) {
-                            val b8 = findViewById<ImageView>(R.id.b8)
-                            b8.setImageResource(R.drawable.o)
-                            tablero2[7] = -1
-                            maquina = false
-                        }
+                    5 -> { if (tablero2[5]==0){
+                        val b6 = findViewById<ImageView>(R.id.b6)
+                        b6.setImageResource(R.drawable.o)
+                        tablero2[5] = -1
+                        maquina = false
+                    }
 
                     }
 
-                    8 -> {
-                        if (tablero2[8] == 0) {
-                            val b9 = findViewById<ImageView>(R.id.b9)
-                            b9.setImageResource(R.drawable.o)
-                            tablero2[8] = -1
-                            maquina = false
-                        }
+                    6 -> { if (tablero2[6]==0){
+                        val b7 = findViewById<ImageView>(R.id.b7)
+                        b7.setImageResource(R.drawable.o)
+                        tablero2[6] = -1
+                        maquina = false
+                    }
+
+                    }
+
+                    7 -> { if (tablero2[7]==0){
+                        val b8 = findViewById<ImageView>(R.id.b8)
+                        b8.setImageResource(R.drawable.o)
+                        tablero2[7] = -1
+                        maquina = false
+                    }
+
+                    }
+
+                    8 -> { if (tablero2[8]==0){
+                        val b9 = findViewById<ImageView>(R.id.b9)
+                        b9.setImageResource(R.drawable.o)
+                        tablero2[8] = -1
+                        maquina = false
+                    }
 
                     }
 
                 }
 
-
             }
 
-        } else {
-            val r = findViewById<Button>(R.id.reset)
-            val mf = findViewById<TextView>(R.id.msj_final)
-            r.visibility = Button.VISIBLE
-            mf.visibility = TextView.VISIBLE
         }
+
 
 
     }
 
-    fun reset() {
+    fun reset(){
         recreate()
     }
 
