85dc16ece6f24b583f1365c8b82c42052e15777e
EAG <mataramosmiguel@gmail.com>
2023-11-07 18:31:29 +0100

    ejercicio 2 hecho

---
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Ej2Recibir.kt
@@ -0,0 +1,21 @@
+package com.example.ejerciciosdesarrollo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Ej2Recibir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ej2_recibir)
+        var palabra = intent.getStringExtra("TEXTO")
+        var numero = intent.getStringExtra("NUMERO")
+
+        val texto = findViewById<TextView>(R.id.texto_recibido)
+        val num = findViewById<TextView>(R.id.num_recibido)
+
+        texto.text = palabra
+        num.text = numero
+
+    }
+}
--- app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio2.kt
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio2.kt
@@ -1,11 +1,32 @@
 package com.example.ejerciciosdesarrollo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class Ejercicio2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio2)
+
+    }
+
+
+    fun iniciarVista(view :View){
+        var intent = Intent(this,Ej2Recibir::class.java)
+
+        val palabra = findViewById<EditText>(R.id.palabra)
+        val numero = findViewById<EditText>(R.id.num)
+
+        var texto = palabra.text.toString()
+        var n = numero.text.toString()
+
+        intent.putExtra("NUMERO",n)
+        intent.putExtra("TEXTO",texto)
+
+        startActivity(intent)
     }
 }
