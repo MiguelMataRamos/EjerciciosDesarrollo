cd34a279680f7a4f493acec626edb2ee21bfe61e
EAG <mataramosmiguel@gmail.com>
2023-11-07 18:45:26 +0100

    ejercicio 3 haciendo las funciones de los botones

---
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Ejercicio3.kt
@@ -0,0 +1,22 @@
+package com.example.ejerciciosdesarrollo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Ejercicio3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio3)
+    }
+
+    var lista : MutableList<String> = mutableListOf()
+
+    fun pasar(view: View) {
+        var palabra = find
+    }
+
+    fun insertar(view: View) {
+
+    }
+}
