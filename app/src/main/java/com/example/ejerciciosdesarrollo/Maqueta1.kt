c77144238b18f643dd901d5563186508b12eaec3
EAG <mataramosmiguel@gmail.com>
2023-11-08 18:40:32 +0100

    Maqueta 1 hecha

---
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Maqueta1.kt
@@ -0,0 +1,11 @@
+package com.example.ejerciciosdesarrollo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Maqueta1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maqueta1)
+    }
+}
