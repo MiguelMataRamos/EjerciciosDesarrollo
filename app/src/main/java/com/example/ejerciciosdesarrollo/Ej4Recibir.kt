f4f1bc3a55c09b8725c0308797580af43fae64be
EAG <mataramosmiguel@gmail.com>
2023-11-14 17:18:14 +0100

    ! bien colocada cagondios

--- app/src/main/java/com/example/ejerciciosdesarrollo/Ej4Recibir.kt
+++ app/src/main/java/com/example/ejerciciosdesarrollo/Ej4Recibir.kt
@@ -14,20 +14,22 @@ class Ej4Recibir : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ej4_recibir)
-        var pais: Ejercicio4.Pais = intent.getParcelableExtra("pais")?: Ejercicio4.Pais("DEFECTO",0,"NADA")
+        val pais: Ejercicio4.Pais = intent.getParcelableExtra("pais")?: Ejercicio4.Pais("DEFECTO",0,"NADA")
 
-        var nombre = findViewById<TextView>(R.id.pais)
-        var habitantes = findViewById<TextView>(R.id.habitantes)
-        var foto = findViewById<ImageView>(R.id.bandera)
+        val nombre = findViewById<TextView>(R.id.pais)
+        val habitantes = findViewById<TextView>(R.id.habitantes)
+        val foto = findViewById<ImageView>(R.id.bandera)
 
 
         nombre.text = pais.nombre
 
         if (pais.bandera=="bandera_espana"){
-            habitantes.text = "Numero de ¡ESPAÑOLES!: " + pais.habitantes.toString()
+            val texto = "Numero de ¡ESPAÑOLES!: " + pais.habitantes.toString()
+            habitantes.text = texto
             foto.setImageResource(R.drawable.bandera_espana)
         }else{
-            habitantes.text = "Numero de Franchutes: " + pais.habitantes.toString()
+            val texto = "Numero de Franchutes: " + pais.habitantes.toString()
+            habitantes.text = texto
             foto.setImageResource(R.drawable.bandera_francia)
         }
 
